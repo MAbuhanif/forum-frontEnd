2f6a69e4d7c5871553b3690b1b075cfa156ccd27
MAbuhanif <mundinoabete@gmail.com>
2024-01-23 22:38:33 +0000

    axios

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosBase = axios.create({
   // baseURL: "http://localhost:5500/api",
-  baseURL: "https://evangadiforumapp.onrender.com/api",
+  baseURL: "https://evangadiforum-d9ty.onrender.com/api",
 });
 export default axiosBase;
